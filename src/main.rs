66dd21fa9d8c04c949f23078bb07fa97489a6516
Elizbeth <elizabethadebayo269@gmail.com>
2024-07-27 21:30:18 +0100

    test running

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+// use actix_files as fs;
+use actix_web::{web, App, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct MyResponse {
+    message: String,
+}
+
+async fn index() -> impl Responder {
+    web::Json(MyResponse {
+        message: "Hello, world!".to_string(),
+    })
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/api", web::get().to(index))
+            .service(fs::Files::new("/", "./frontend").index_file("index.html"))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
